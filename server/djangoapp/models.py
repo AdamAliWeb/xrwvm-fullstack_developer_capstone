8f6f12dc7ae8eb8ccf3310fd2def95fa881beaca
AdamAliWeb <adamstar080904@gmail.com>
2026-05-29 01:45:10 -0400

    Fixing Python Linting 1 Try

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -18,6 +18,7 @@ class CarMake(models.Model):
 
     def __str__(self):
         return self.name
+
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -39,9 +40,10 @@ class CarModel(models.Model):
     type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
     year = models.IntegerField(default=2023,
     validators=[
-        MaxValueValidator(2023),
-        MinValueValidator(2015)
+    MaxValueValidator(2023),
+    MinValueValidator(2015)
     ])
 
     def __str__(self):
         return self.name
+        
