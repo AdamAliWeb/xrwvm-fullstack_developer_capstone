22e4cf473453297fafbc1dc8674f17ad6fb99adf
AdamAliWeb <adamstar080904@gmail.com>
2026-05-31 12:49:31 -0400

    Containerized the application

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -16,8 +16,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 from .models import CarMake, CarModel
 from .populate import initiate
-from .restapis import get_request, analyze_review_sentiments
-# from .restapis import get_request, analyze_review_sentiments, post_review
+# from .restapis import get_request, analyze_review_sentiments
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 
 # Get an instance of a logger
@@ -132,10 +132,10 @@ def get_dealer_reviews(request, dealer_id):
 # Create a `add_review` view to submit a review
 def add_review(request):
     if (request.user.is_anonymous is False):
-        # data = json.loads(request.body)
+        data = json.loads(request.body)
 
         try:
-            # response = post_review(data)
+            response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception as err:
             return JsonResponse(
